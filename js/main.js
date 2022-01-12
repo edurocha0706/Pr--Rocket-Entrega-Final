75dd51ef322d34aa59ddd102bb27dd9491e6a892
Eduardo de Almeida Rocha <eduardorochaa@gmail.com>
2022-01-12 00:29:11 -0300

    Corrige a função de ler os cards de novos agentes e adiciona nova função de ler os cards com as informações do banco de dados

--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@ window.onload=function(){
 
   $popupClose.addEventListener('click', function(){
     $popupWrapper.style.display = 'none';
+    document.getElementById('content-data-id').remove();
+    document.getElementById('content-image-id').remove();
   });
 
   var $seeAllDiv = document.querySelector('.seeall-div-text')
@@ -33,6 +35,8 @@ window.onload=function(){
 
   var $saveButton = document.querySelector('.save-button');
 
+// --- INÍCIO --- Cria novos agentes.
+
   var name = '';
   var function0 = 0;
   var description = '';
@@ -47,13 +51,9 @@ window.onload=function(){
   var image = '';
 
   var arrNewAgents = [];
-
   var newAgentCreated;
   var textToAdd= '';
-  var agentsTextToView= '';
-  
   var count = 0;
-  var arrCardText = [];
 
   $saveButton.addEventListener('click', function(){
     name = document.getElementById('name-field').value;
@@ -79,37 +79,29 @@ window.onload=function(){
     
     arrNewAgents.push(newAgentCreated);
 
-    textToAdd = '<div class="agent-div" id="agent-div-id"><img class="agent-img" src="'+arrNewAgents[count].image+'" alt="agente '+arrNewAgents[count].name+'"><div class="agent agent'+count+'"><p>'+arrNewAgents[count].name+'</p></div></div>';
+    textToAdd = '<div class="agent-div" id="agent-div-id"><img class="agent-img" src="'+arrNewAgents[count].image+'" alt="agente '+arrNewAgents[count].name+'"><div class="agent newAgent'+count+'"><p>'+arrNewAgents[count].name+'</p></div></div>';
     document.getElementById('agents-flexbox-id').insertAdjacentHTML('beforeend',textToAdd)
 
-    var agentsTextToView = '<div class="content-data" id="content-data-id"><h3>FUNÇÃO</h3><p class="function">'+arrNewAgents[count].function0+'</p><p class="description">'+arrNewAgents[count].description+'</p><h3>HABILIDADES</h3><div class="skills-flexbox"><div class="types-of-weapons"><p>'+arrNewAgents[count].skills[0].type1+'</p><p>'+arrNewAgents[count].skills[1].type2+'</p><p>'+arrNewAgents[count].skills[2].type3+'</p><p>'+arrNewAgents[count].skills[3].type4+'</p></div><div class="damages"><p>'+arrNewAgents[count].skills[0].damage1+' Dano</p><p>'+arrNewAgents[count].skills[1].damage2+' Dano</p><p>'+arrNewAgents[count].skills[2].damage3+' Dano</p><p>'+arrNewAgents[count].skills[3].damage4+' Dano</p></div></div></div><div class="content-image" id="content-image-id"><img class="agent-img" src="'+arrNewAgents[count].image+'" alt="imagem do agente"></div>';
+    showNewAgents(count);
 
-    arrCardText.push(agentsTextToView);
-    
-    if(count !== 0){
-      document.getElementById('content-data-id').remove();
-      document.getElementById('content-image-id').remove();
-    }
-
-    document.getElementById('popup-content-flexbox-id').insertAdjacentHTML('beforeend',arrCardText[count]);
-
-    for(var i=0; i<arrCardText.length; i++){
-      var $agentClassName = document.querySelector('.agent'+count);
-      
-      if('.agent'+count === '.agent'+i){
-        
-        $agentClassName.addEventListener('click', function(){
-        $popupWrapper.style.display = 'block';
-        });
-      } else {
-        
-        $agentClassName.addEventListener('click', function(){
-        $popupWrapper.style.display = 'block';
-        });
-      }
-    }
-    count++;
+    count++;    
   });
+  // --- FIM --- Cria novos agentes.
+  
+  // --- INÍCIO --- Mostra os novos agentes criados.
+
+  function showNewAgents(agtX){
+    var agt = agtX;
+    var $agentId = document.querySelector('.newAgent'+agtX);
+    
+    var textToView = '<div class="content-data" id="content-data-id"><h3>FUNÇÃO</h3><p class="function">'+arrNewAgents[agt].function0+'</p><p class="description">'+arrNewAgents[agt].description+'</p><h3>HABILIDADES</h3><div class="skills-flexbox"><div class="types-of-weapons"><p>'+arrNewAgents[agt].skills[0].type1+'</p><p>'+arrNewAgents[agt].skills[1].type2+'</p><p>'+arrNewAgents[agt].skills[2].type3+'</p><p>'+arrNewAgents[agt].skills[3].type4+'</p></div><div class="damages"><p>'+arrNewAgents[agt].skills[0].damage1+' Dano</p><p>'+arrNewAgents[agt].skills[1].damage2+' Dano</p><p>'+arrNewAgents[agt].skills[2].damage3+' Dano</p><p>'+arrNewAgents[agt].skills[3].damage4+' Dano</p></div></div></div><div class="content-image" id="content-image-id"><img class="agent-img" src="'+arrNewAgents[agt].image+'" alt="imagem do agente"></div>';
+    $agentId.addEventListener('click', function(){
+      document.getElementById('popup-content-flexbox-id').insertAdjacentHTML('beforeend',textToView);
+    $popupWrapper.style.display = 'block';
+    })
+  }
+
+  // --- FIM --- Mostra os novos agentes criados.
 
   var $cancelButton = document.querySelector('.cancel-button');
 
@@ -143,5 +135,328 @@ window.onload=function(){
     document.getElementById('image-field').value = '';
   }
 
+  const json = `[{
+    "data": {
+      "agents": [
+        {
+          "name": "Jett",
+          "function": "Duelista",
+          "description": "Devonshire rex. Devonshire rex birman but sphynx savannah panther lynx british shorthair. Tiger. Burmese british shorthair savannah but ocicat siamese for norwegian forest. American shorthair cornish rex american bobtail puma sphynx. Tabby munchkin savannah turkish angora, siamese yet tabby. Siberian grimalkin, birman so lynx but abyssinian tomcat. Ocicat scottish fold but devonshire rex.",
+          "skills": [
+            {
+              "type": 1,
+              "damage": 5
+            },
+            {
+              "type": 2,
+              "damage": 8
+            },
+            {
+              "type": 3,
+              "damage": 10
+            },
+            {
+              "type": 4,
+              "damage": 30
+            }
+          ],
+          "image": "./agents/jett.png"
+        },
+        {
+          "name": "Raze",
+          "function": "Sentinela",
+          "description": "Devonshire rex. Devonshire rex birman but sphynx savannah panther lynx british shorthair. Tiger. Burmese british shorthair savannah but ocicat siamese for norwegian forest. American shorthair cornish rex american bobtail puma sphynx. Tabby munchkin savannah turkish angora, siamese yet tabby. Siberian grimalkin, birman so lynx but abyssinian tomcat. Ocicat scottish fold but devonshire rex.",
+          "skills": [
+            {
+              "type": 1,
+              "damage": 6
+            },
+            {
+              "type": 2,
+              "damage": 3
+            },
+            {
+              "type": 3,
+              "damage": 13
+            },
+            {
+              "type": 4,
+              "damage": 25
+            }
+          ],
+          "image": "./agents/raze.png"
+        },
+        {
+          "name": "Breach",
+          "function": "Duelista",
+          "description": "Devonshire rex. Devonshire rex birman but sphynx savannah panther lynx british shorthair. Tiger. Burmese british shorthair savannah but ocicat siamese for norwegian forest. American shorthair cornish rex american bobtail puma sphynx. Tabby munchkin savannah turkish angora, siamese yet tabby. Siberian grimalkin, birman so lynx but abyssinian tomcat. Ocicat scottish fold but devonshire rex.",
+          "skills": [
+            {
+              "type": 1,
+              "damage": 5
+            },
+            {
+              "type": 2,
+              "damage": 8
+            },
+            {
+              "type": 3,
+              "damage": 15
+            },
+            {
+              "type": 4,
+              "damage": 10
+            }
+          ],
+          "image": "./agents/breach.png"
+        },
+        {
+          "name": "Omen",
+          "function": "Controlador",
+          "description": "Devonshire rex. Devonshire rex birman but sphynx savannah panther lynx british shorthair. Tiger. Burmese british shorthair savannah but ocicat siamese for norwegian forest. American shorthair cornish rex american bobtail puma sphynx. Tabby munchkin savannah turkish angora, siamese yet tabby. Siberian grimalkin, birman so lynx but abyssinian tomcat. Ocicat scottish fold but devonshire rex.",
+          "skills": [
+            {
+              "type": 1,
+              "damage": 9
+            },
+            {
+              "type": 2,
+              "damage": 5
+            },
+            {
+              "type": 3,
+              "damage": 10
+            },
+            {
+              "type": 4,
+              "damage": 20
+            }
+          ],
+          "image": "./agents/omen.png"
+        },
+        {
+          "name": "Brimstone",
+          "function": "Iniciador",
+          "description": "Devonshire rex. Devonshire rex birman but sphynx savannah panther lynx british shorthair. Tiger. Burmese british shorthair savannah but ocicat siamese for norwegian forest. American shorthair cornish rex american bobtail puma sphynx. Tabby munchkin savannah turkish angora, siamese yet tabby. Siberian grimalkin, birman so lynx but abyssinian tomcat. Ocicat scottish fold but devonshire rex.",
+          "skills": [
+            {
+              "type": 1,
+              "damage": 5
+            },
+            {
+              "type": 2,
+              "damage": 5
+            },
+            {
+              "type": 3,
+              "damage": 10
+            },
+            {
+              "type": 4,
+              "damage": 20
+            }
+          ],
+          "image": "./agents/brimstone.png"
+        },
+        {
+          "name": "Phoenix",
+          "function": "Duelista",
+          "description": "Devonshire rex. Devonshire rex birman but sphynx savannah panther lynx british shorthair. Tiger. Burmese british shorthair savannah but ocicat siamese for norwegian forest. American shorthair cornish rex american bobtail puma sphynx. Tabby munchkin savannah turkish angora, siamese yet tabby. Siberian grimalkin, birman so lynx but abyssinian tomcat. Ocicat scottish fold but devonshire rex.",
+          "skills": [
+            {
+              "type": 1,
+              "damage": 15
+            },
+            {
+              "type": 2,
+              "damage": 8
+            },
+            {
+              "type": 3,
+              "damage": 10
+            },
+            {
+              "type": 4,
+              "damage": 30
+            }
+          ],
+          "image": "./agents/phoenix.png"
+        },
+        {
+          "name": "Sage",
+          "function": "Iniciador",
+          "description": "Devonshire rex. Devonshire rex birman but sphynx savannah panther lynx british shorthair. Tiger. Burmese british shorthair savannah but ocicat siamese for norwegian forest. American shorthair cornish rex american bobtail puma sphynx. Tabby munchkin savannah turkish angora, siamese yet tabby. Siberian grimalkin, birman so lynx but abyssinian tomcat. Ocicat scottish fold but devonshire rex.",
+          "skills": [
+            {
+              "type": 1,
+              "damage": 25
+            },
+            {
+              "type": 2,
+              "damage": 4
+            },
+            {
+              "type": 3,
+              "damage": 15
+            },
+            {
+              "type": 4,
+              "damage": 25
+            }
+          ],
+          "image": "./agents/sage.png"
+        },
+        {
+          "name": "Sova",
+          "function": "Duelista",
+          "description": "Devonshire rex. Devonshire rex birman but sphynx savannah panther lynx british shorthair. Tiger. Burmese british shorthair savannah but ocicat siamese for norwegian forest. American shorthair cornish rex american bobtail puma sphynx. Tabby munchkin savannah turkish angora, siamese yet tabby. Siberian grimalkin, birman so lynx but abyssinian tomcat. Ocicat scottish fold but devonshire rex.",
+          "skills": [
+            {
+              "type": 1,
+              "damage": 5
+            },
+            {
+              "type": 2,
+              "damage": 10
+            },
+            {
+              "type": 3,
+              "damage": 10
+            },
+            {
+              "type": 4,
+              "damage": 40
+            }
+          ],
+          "image": "./agents/sova.png"
+        },
+        {
+          "name": "Viper",
+          "function": "Duelista",
+          "description": "Devonshire rex. Devonshire rex birman but sphynx savannah panther lynx british shorthair. Tiger. Burmese british shorthair savannah but ocicat siamese for norwegian forest. American shorthair cornish rex american bobtail puma sphynx. Tabby munchkin savannah turkish angora, siamese yet tabby. Siberian grimalkin, birman so lynx but abyssinian tomcat. Ocicat scottish fold but devonshire rex.",
+          "skills": [
+            {
+              "type": 1,
+              "damage": 12
+            },
+            {
+              "type": 2,
+              "damage": 18
+            },
+            {
+              "type": 3,
+              "damage": 20
+            },
+            {
+              "type": 4,
+              "damage": 15
+            }
+          ],
+          "image": "./agents/viper.png"
+        },
+        {
+          "name": "Cypher",
+          "function": "Controlador",
+          "description": "Devonshire rex. Devonshire rex birman but sphynx savannah panther lynx british shorthair. Tiger. Burmese british shorthair savannah but ocicat siamese for norwegian forest. American shorthair cornish rex american bobtail puma sphynx. Tabby munchkin savannah turkish angora, siamese yet tabby. Siberian grimalkin, birman so lynx but abyssinian tomcat. Ocicat scottish fold but devonshire rex.",
+          "skills": [
+            {
+              "type": 1,
+              "damage": 10
+            },
+            {
+              "type": 2,
+              "damage": 15
+            },
+            {
+              "type": 3,
+              "damage": 10
+            },
+            {
+              "type": 4,
+              "damage": 20
+            }
+          ],
+          "image": "./agents/cypher.png"
+        },
+        {
+          "name": "Reyna",
+          "function": "Iniciador",
+          "description": "Devonshire rex. Devonshire rex birman but sphynx savannah panther lynx british shorthair. Tiger. Burmese british shorthair savannah but ocicat siamese for norwegian forest. American shorthair cornish rex american bobtail puma sphynx. Tabby munchkin savannah turkish angora, siamese yet tabby. Siberian grimalkin, birman so lynx but abyssinian tomcat. Ocicat scottish fold but devonshire rex.",
+          "skills": [
+            {
+              "type": 1,
+              "damage": 14
+            },
+            {
+              "type": 2,
+              "damage": 18
+            },
+            {
+              "type": 3,
+              "damage": 27
+            },
+            {
+              "type": 4,
+              "damage": 45
+            }
+          ],
+          "image": "./agents/reyna.png"
+        }
+      ],
+      "guns": {
+        "primaryGun": [
+          "./guns/gun6.png",
+          "./guns/gun7.png",
+          "./guns/gun8.png",
+          "./guns/gun11.png",
+          "./guns/gun12.png"
+        ],
+        "secondaryGun": [
+          "./guns/gun14.png",
+          "./guns/gun15.png",
+          "./guns/gun13.png",
+          "./guns/gun9.png",
+          "./guns/gun10.png"
+        ],
+        "steel": [
+          "./guns/gun1.png",
+          "./guns/gun2.png",
+          "./guns/gun3.png",
+          "./guns/gun4.png",
+          "./guns/gun5.png"
+        ]
+      },
+      "maps": [
+        "./maps/map1.png",
+        "./maps/map2.png",
+        "./maps/map3.png",
+        "./maps/map4.png",
+        "./maps/map5.png"
+      ]
+    }
+  }]`
+
+    // --- INÍCIO --- Mostra os agentes que constam no banco de dados.
+
+    let allagents = JSON.parse(json);
+
+    var numberOfAgents = 11;
+  
+    for(var i=0;i<numberOfAgents;i++){
+      showAgentOfData(i);
+    }
+    
+    function showAgentOfData(agtX){
+      var agt = agtX;
+      var $agentId = document.querySelector('.agent'+agtX);
+      
+      var textToView2 = '<div class="content-data" id="content-data-id"><h3>FUNÇÃO</h3><p class="function">'+allagents[0].data.agents[agt].function+'</p><p class="description">'+allagents[0].data.agents[agt].description+'</p><h3>HABILIDADES</h3><div class="skills-flexbox"><div class="types-of-weapons"><p>'+'Arma branca'+'</p><p>'+'Arma primária'+'</p><p>'+'Arma secundária'+'</p><p>'+'Especial'+'</p></div><div class="damages"><p>'+allagents[0].data.agents[agt].skills[0].damage+' Dano</p><p>'+allagents[0].data.agents[agt].skills[1].damage+' Dano</p><p>'+allagents[0].data.agents[agt].skills[2].damage+' Dano</p><p>'+allagents[0].data.agents[agt].skills[3].damage+' Dano</p></div></div></div><div class="content-image" id="content-image-id"><img class="agent-img" src="'+allagents[0].data.agents[agt].image+'" alt="imagem do agente"></div>';
+      $agentId.addEventListener('click', function(){
+        document.getElementById('popup-content-flexbox-id').insertAdjacentHTML('beforeend',textToView2);
+      $popupWrapper.style.display = 'block';
+      })
+    }
+  
+    // --- FIM --- Mostra os agentes que constam no banco de dados.
 }
 
